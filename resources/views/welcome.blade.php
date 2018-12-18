4299bae2929e3e550479226556e00514f806ed38
abnerGP <abner.gp@gmail.com>
2018-12-18 12:55:29 -0600

    Cambiar mensaje

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Aprendiendo a usar Git
                 </div>
 
                 <div class="links">
